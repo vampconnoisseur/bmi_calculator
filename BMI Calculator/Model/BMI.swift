2c222b630f994345cbc9d8bb33db25610944fce9
Jaiditya Batra <jaiditya7318@gmail.com>
2023-06-21 15:27:32 +0530

    Update BMI Calculator

---
+++ BMI Calculator/Model/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Jaiditya Batra on 21/06/23.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
